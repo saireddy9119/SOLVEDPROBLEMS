cea8273b69403a0491a2b8b800273a8d4cb6e6ef
TAPPETA SAI NARASIMHA REDDY <107350833+saireddy9119@users.noreply.github.com>
2024-05-17 23:21:53 +0530

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,8 +1,9 @@
+// Implement a function to check if a given string is a palindrome.
 function isPalindrome(num) {
     let numStr = num.toString();
     let result = numStr.split('').reverse().join('');
     return numStr === result;
 
 }
-console.log(isPalindrome("aioa")); // true 
-console.log(isPalindrome(9119)); // false
+console.log(isPalindrome("aiia")); // true 
+console.log(isPalindrome("aioa")); // false
